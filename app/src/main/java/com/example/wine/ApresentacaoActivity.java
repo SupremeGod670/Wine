201c5a212e3cb77e9684ea5171477fd13e5d8ea1
Gabriel Goulart <gabriel.gsouzza6@gmail.com>
2025-04-30 19:41:27 -0300

    Start Commit

--- app/src/main/java/com/example/wine/ApresentacaoActivity.java
+++ app/src/main/java/com/example/wine/ApresentacaoActivity.java
@@ -1,6 +1,15 @@
 package com.example.wine;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ApresentacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.apresentacao);
+    }
 }
