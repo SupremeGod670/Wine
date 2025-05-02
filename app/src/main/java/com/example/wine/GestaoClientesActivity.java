dd56b7634b7ab582ca9fdc45f637ed5b4150e46f
Gabriel Goulart <gabriel.gsouzza6@gmail.com>
2025-05-02 20:24:32 -0300

    Start Commit

---
+++ app/src/main/java/com/example/wine/GestaoClientesActivity.java
@@ -0,0 +1,6 @@
+package com.example.wine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GestaoClientesActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/wine/VendasExternasActivity.java
@@ -0,0 +1,6 @@
+package com.example.wine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VendasExternasActivity extends AppCompatActivity {
+}
