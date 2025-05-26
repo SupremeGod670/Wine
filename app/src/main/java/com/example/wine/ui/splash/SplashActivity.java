bbdf5f019edc833e089703c906663bd13edb5271
Gabriel Goulart <gabriel.gsouzza6@gmail.com>
2025-05-26 20:35:49 -0300

    Icone do APP, e splash.xml criados e adicionados.

--- app/src/main/java/com/example/wine/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/wine/ui/splash/SplashActivity.java
@@ -1,17 +1,48 @@
 package com.example.wine.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wine.R;
+import com.example.wine.ui.login.LoginAPPActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView splash;
+    private Animation anim;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        splash = findViewById(R.id.splash);
+        anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashActivity.this, LoginAPPActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        splash.setAnimation(anim);
+        anim.start();
+
     }
 }
