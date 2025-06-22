b38c521a0f62953d1e03115a2c7b9ad4eb5d562a
xLeotte <victorleottegaz@gmail.com>
2025-06-22 15:06:12 -0300

    Serachview funcionando
    Implementação do searchview no WinelistActivity

--- app/src/main/java/com/example/wine/adapter/WineAdapter.java
+++ app/src/main/java/com/example/wine/adapter/WineAdapter.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,24 +13,25 @@ import com.example.wine.R;
 
 import java.util.ArrayList;
 
-public class WineAdapter extends BaseAdapter {
-
-    private final ArrayList<WineModel> listWine;
+public class WineAdapter extends BaseAdapter implements Filterable {
     private final Activity activity;
+    private ArrayList<WineModel> listWine;
+    private ArrayList<WineModel> filteredList;
 
     public WineAdapter(Activity activity, ArrayList<WineModel> listWine) {
         this.activity = activity;
         this.listWine = listWine;
+        this.filteredList = new ArrayList<>(listWine);
     }
 
     @Override
     public int getCount() {
-        return listWine.size();
+        return filteredList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return listWine.get(position);
+        return filteredList.get(position);
     }
 
     @Override
@@ -42,9 +45,8 @@ public class WineAdapter extends BaseAdapter {
             view = activity.getLayoutInflater().inflate(R.layout.wine, viewGroup, false);
         }
 
-        WineModel wine = listWine.get(position);
+        WineModel wine = filteredList.get(position);
 
-        ImageView imageWine = view.findViewById(R.id.wine);
         TextView nome = view.findViewById(R.id.nome);
         TextView varietal = view.findViewById(R.id.varietal);
         TextView doc = view.findViewById(R.id.doc);
@@ -59,4 +61,42 @@ public class WineAdapter extends BaseAdapter {
 
         return view;
     }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    private final Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<WineModel> filtered = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filtered.addAll(listWine);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (WineModel item : listWine) {
+                    if (item.getNome().toLowerCase().contains(filterPattern) ||
+                            item.getVarietal().toLowerCase().contains(filterPattern) ||
+                            item.getVinicola().toLowerCase().contains(filterPattern) ||
+                            item.getDoc().toLowerCase().contains(filterPattern) ||
+                            item.getSafra().toLowerCase().contains(filterPattern)) {
+                        filtered.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filtered;
+            results.count = filtered.size();
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredList.clear();
+            filteredList.addAll((ArrayList) results.values);
+            notifyDataSetChanged();
+        }
+    };
 }
